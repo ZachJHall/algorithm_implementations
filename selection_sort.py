ce92efdd3f57f10893c311d849befe39d9b6f32c
ZachJHall <zachjohnhall@gmail.com>
2019-10-16 21:22:18 -0500

    Initial commit

---
+++ selection_sort.py
@@ -0,0 +1,18 @@
+import random
+
+sorted = []
+
+unsorted = [random.randint(0,100) for i in range(10)]
+
+minimum = unsorted[0]
+
+while len(unsorted) > 0:
+    minimum = unsorted[0]
+    for number in unsorted:
+        if number <= minimum:
+            minimum = number
+    unsorted.remove(minimum)
+    sorted.append(minimum)
+
+
+print(sorted)
